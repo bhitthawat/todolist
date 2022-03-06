b996d815bf77353e33113e21287ac40b56b99be5
bhitthawat <bhitthawat@builk.com>
2022-03-07 00:20:32 +0700

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const urlRoot = "http://localhost:5000/";
 
 function App() {
+  const [data, setData] = React.useState([]);
+  const [input, setInput] = React.useState("");
+
+  function GetListItem() {
+    fetch(urlRoot)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }
+
+  function CreateListitem() {
+    fetch(urlRoot + input, { method: "POST" }).then(() => GetListItem());
+    setInput("");
+  }
+
+  function DeleteListitem(item) {
+    fetch(urlRoot + item, { method: "DELETE" }).then(() => GetListItem());
+  }
+
+  function updateInputValue(e) {
+    setInput(e.target.value);
+  }
+
+  React.useEffect(() => {
+    GetListItem();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="col-8 py-5 vh-100 mx-auto">
+      <h1 className="text-center mb-5">To-Do-List</h1>
+      <ul
+        className="list-group mb-4 overflow-auto"
+        style={{ maxHeight: "60vh" }}
+      >
+        {data.map((item) => (
+          <li className="list-group-item d-flex justify-content-between align-items-center">
+            <p className="col-6 m-0">{item.name}</p>
+            <button
+              className="w-10 btn btn-danger"
+              onClick={() => DeleteListitem(item._id)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <input
+        type="text"
+        class="form-control mb-3"
+        placeholder='Press "Enter" to Submit'
+        onChange={updateInputValue}
+        value={input}
+        onKeyPress={(e) => e.key === "Enter" && CreateListitem()}
+      ></input>
     </div>
   );
 }
